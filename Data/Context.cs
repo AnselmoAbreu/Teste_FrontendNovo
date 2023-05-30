9bf219b558001690829e80d74a9bdd30a2a53196
Anselmo <anselmo.mdea@gmail.com>
2023-05-30 14:51:13 -0300

    Add project files.

---
+++ Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Teste_Frontend.Models;
+
+namespace Teste_Frontend.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Produto> Pessoas { get; set; }
+    }
+}
---
+++ Data/Teste_FrontendContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Teste_Frontend.Models;
+
+namespace Teste_Frontend.Data
+{
+    public class Teste_FrontendContext : DbContext
+    {
+        public Teste_FrontendContext (DbContextOptions<Teste_FrontendContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teste_Frontend.Models.Produto> Produto { get; set; }
+    }
+}
